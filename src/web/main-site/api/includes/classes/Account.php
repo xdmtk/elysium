5143532c8419cda27cc00ebf1f680d96170d11cd
Sebastian Enciso <Sebastianbabble@gmail.com>
2020-03-27 08:07:46 -0700

    web/#24-no-email-verify-yet-tester

--- src/web/main-site/api/includes/classes/Account.php
+++ src/web/main-site/api/includes/classes/Account.php
@@ -1,31 +1,54 @@
 <?php 
-	Class Account{
+	include("../api/includes/classes/Database.php");
 
-		public function register(){
+	class Account{
 
+		/**
+		* @param User parameters from login
+		* If all the fields are valid then it creates
+		* a database object and uploads the user to 
+		* DB.
+		*/
+		public function register($userName,$password,$email){
+
+			if($this->validateUserName($userName) &&
+				$this->validateEmail($email) &&
+				$this->validatePassword($password)){
+				$dataBase = new Database();
+				$dataBase->verifyAndRegister($userName,$password,$email,'Y');
+			}
 		}
 
-		
-		private function validateUsername(){
+		/**
+		* @param users name  
+		* Function to validate username and make sure its 
+		* a certain length
+		* @return true if its a valid username 
+		*/
+		public function validateUserName($userName){
 
+			if(strlen($userName) > 25 || strlen($userName) < 5 ){
+				echo 'Username must be between 5 and 25 characters';
+				return false;
+			}
+			return true;
 		}
-		private function validateFirstName(){
 
-		}
-		private function validateLastName(){
+		/**
+		* @param users password 
+		* Function to validate password and make sure
+		* it's strong enough
+		* @return true if its a valid password
+		*/ 
+		public function validatePassword($password){
 
-		}
-		private function validateEmail(){
-
-		}
-		private function validatePassword($pw){
-			
-			$password = $pw;
-			$uppercase = preg_match('/[A-Z]/', $password);
+			//1. Check if password contains at least one values
+	        $password = $password;
+	        $uppercase = preg_match('/[A-Z]/', $password);
 	        $lowercase = preg_match('/[a-z]/', $password);
 	        $number = preg_match('/[0-9]/', $password);
 	        $specialChars = preg_match('/[^\w]/', $password);
-
+	        
 	        //2. If password doesn't meet all requirments then return false 
 	        if(!$uppercase || !$lowercase || !$number || !$specialChars || 
 	            strlen($password) < 8) {
@@ -35,7 +58,32 @@
 	        else{
 	            echo 'Strong password';
 	            return true;
-	        }  
+	        }
 		}
+
+		public function sendEmailVerify($email){
+			
+			 $email = htmlspecialchars(strip_tags($email));
+			 $to = "sebastianbabble@gmail.com";
+			 $subject = "Please confirm your account with Elysium";
+			 $headers = "From: staff@elysium-project.net\r\n";
+			 $message = "Yo welcome to elysium";
+			 mail($to, $subject,$message,$headers);
+		}
+
+		/**
+		* @param users email 
+		* Function to validate a person email making sure
+		* it includes the chars @ and .com
+		* @return true if its a valid email  
+		*/
+		public function validateEmail($email){
+			if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+				echo 'Not a valid email address';
+				return false;
+			}
+			return true;
+		}
+		
 	}
  ?>
---
+++ src/web/main-site/api/includes/classes/Database.php
@@ -0,0 +1,124 @@
+<?php
+include("../api/includes/classes/Environment.php");
+class Database{
+
+    private $conn;
+    
+    /*
+    * Connect to database and check 
+    * for errors
+    */
+    public function __construct(){
+         $this->conn = null;
+
+        //1. Connect to DB and check for valid connection
+        try{
+            $this->conn = new PDO('mysql:host=' . Environment::get("DB_HOST") . ';dbname=' . Environment::get("DB_NAME"), Environment::get("DB_USER"), Environment::get("DB_PASS") );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        } catch(PDOException $e){
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+    /**
+    * @param value of userName 
+    * Check if user exists in DB and 
+    * @return true if user doesn't exist in database
+    */
+    private function userExists($userName){
+        
+        $chkUserName = $this->conn->prepare("SELECT username FROM clients WHERE username=:username");
+        $chkUserName->bindParam(":username", $userName);
+        $chkUserName->execute();
+
+        if($chkUserName->rowCount() > 0){
+            echo 'Error: User already exists';
+            return false;
+        }
+        return true;
+    }
+
+    /**
+    * @param users email
+    * Checks if user already exists in the database
+    * and returns true if they are and false
+    * if they aren't
+    * @return true if email doesn't exist in database
+    */
+    private function emailExists($email){
+
+        //1.Prepare statement for checking
+        $chkEmail = $this->conn->prepare("SELECT email FROM clients WHERE email=:email");
+        $chkEmail->bindParam(":email", $email);
+        $chkEmail->execute();
+
+        //2.Check if table has a row with current username 
+        if($chkEmail->rowCount() > 0){
+            echo 'Error: User already exists';
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+    *@param Db connection with all table field values
+    * takes the values and binds them to the query string
+    * @return Db connection with binded query 
+    */
+    private function bindQuery($stmt,$userName,$password,$email,$verify){
+        $stmt->bindValue(":id", null, PDO::PARAM_INT);
+        $stmt->bindParam(":username", $userName);
+        $stmt->bindParam(":password", $password);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":verify", $verify);
+
+        return $stmt;
+    }
+
+    /** 
+     * Create Post
+     * @param Table fields  
+     * Creates the query and validates if 
+     * username or email exists in DB
+     */
+    private function create($userName,$password,$email,$verify){
+        
+        //1. Create Query
+        $query = "INSERT INTO " .
+              Environment::get("DB_TABLE") . "
+                SET
+                  id=:id,
+                  username=:username,
+                  password=:password,
+                  email=:email,
+                  verify=:verify";
+
+        //2.Prepare query 
+        $stmt = $this->conn->prepare($query);
+
+        $verify = htmlspecialchars_decode(strip_tags($verify));
+
+        //6. Bind Data
+        $stmt = $this->bindQuery($stmt,$userName,$password,$email,$verify);
+
+        //7. Execute Query
+        if($stmt->execute())
+            echo 'Successful post';
+        else
+            echo 'There must have been a problem with the post';
+    }
+        
+    /**
+    * @param Table fields
+    * If username and email aren't taken then register user to db
+    */
+    public function verifyAndRegister($userName, $password, $email, $verified){
+       if($this->userExists($userName) && $this->emailExists($email))
+             $this->create($userName, $password, $email, $verified);
+  }    
+}
+
